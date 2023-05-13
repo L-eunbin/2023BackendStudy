07d58956378d2f81d3212fa764ed7f494c3bda9b
Lee Eunbin <enbin02@gmail.com>
2023-05-13 22:10:16 +0900

    first commit

---
+++ StartHtml/src/main/java/com/likelion/StartHtml/StartHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.StartHtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartHtmlApplication.class, args);
+	}
+
+}
